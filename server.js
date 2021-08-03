55047fd9b565b60788938d2cb266b210543b48fa
chabiri <chyb627@naver.com>
2021-08-03 18:10:13 +0900

    ToDoApp 입니다

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app.use(bodyParser.urlencoded({extended: true}))
 const MongoClient = require('mongodb').MongoClient;
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public')); //static파일을 보관하기위해 public폴더를 쓰겠다
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 
 
 var db;
@@ -114,6 +116,132 @@ app.get('/detail/:id', function(요청, 응답){
 });
 
 
-app.get('/edit', function(요청, 응답){
-    응답.render('edit.ejs')
+app.get('/edit/:id', function(요청, 응답){
+    
+    db.collection('post').findOne({_id : parseInt(요청.params.id)}, function(에러, 결과){
+        응답.render('edit.ejs',{ post : 결과 }) //파라미터중 :id 번 게시물의 제목, 날짜
+    })
+})
+
+app.put('/edit', function(요청, 결과){
+    //폼에담긴 제목데이터, 날짜데이터를 가지고 
+     //db.collection에다가 업데이트함
+    db.collection('post').updateOne( {_id : parseInt(요청.body.id) }, {$set : { 제목 : 요청.body.title , 날짜 : 요청.body.date }}, function(){
+      console.log('수정완료')
+      응답.redirect('/list')
+    });
+  });
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session'); 
+
+app.use(session({secret : '!cha159632', resave : true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+app.get('/login', function(요청, 응답){
+    응답.render('login.ejs')
+  });
+
+  app.post('/login', passport.authenticate('local', {failureRedirect : '/fail'}), function(요청, 응답){
+    응답.redirect('/')
+  });
+
+app.get('/mypage', 로그인했니, function (요청, 응답) {
+    console.log(요청.user);
+    응답.render('mypage.ejs', {사용자 : 요청.user})
+  })
+  
+  function 로그인했니(요청, 응답, next) {
+    if (요청.user) {
+      next()
+    } else {
+      응답.send('로그인안하셨는데요?')
+    }
+  }
+
+
+
+  passport.use(new LocalStrategy({
+    usernameField: 'id',
+    passwordField: 'pw',
+    session: true,
+    passReqToCallback: false,
+  }, function (입력한아이디, 입력한비번, done) {
+    //console.log(입력한아이디, 입력한비번);
+    db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+      if (에러) return done(에러)
+  
+      if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+      if (입력한비번 == 결과.pw) {
+        return done(null, 결과)
+      } else {
+        return done(null, false, { message: '비번틀렸어요' })
+      }
+    })
+  }));
+
+  passport.serializeUser(function (user, done) {
+    done(null, user.id)
+  });
+  
+  passport.deserializeUser(function (아이디, done) {
+    db.collection('login').findOne({ id: 아이디 }, function (에러, 결과) {
+      done(null, 결과)
+    })
+  }); 
+
+  app.post('/add', function (요청, 응답) {
+    console.log(요청.user._id)
+    응답.send('전송완료');
+    db.collection('counter').findOne({ name: '게시물갯수' }, function (에러, 결과) {
+      var 총게시물갯수 = 결과.totalPost;
+      var post = { _id: 총게시물갯수 + 1, 작성자: 요청.user._id , 제목: 요청.body.title, 날짜: 요청.body.date }
+      db.collection('post').insertOne( post , function (에러, 결과) {
+        db.collection('counter').updateOne({ name: '게시물갯수' }, { $inc: { totalPost: 1 } }, function (에러, 결과) {
+          if (에러) { return console.log(에러) }
+        })
+      });
+    });
+  });
+
+
+
+  app.get('/search', (요청, 응답)=>{
+    console.log(요청.query);
+    db.collection('post').find({제목 : 요청.query.value}).toArray((에러, 결과)=>{
+      console.log(결과)
+    })
+  })
+
+
+  app.use('/shop', require('./routes/shop.js') );
+  app.use('/board/sub', require('./routes/board.js') );
+
+  app.get('/upload', function(요청, 응답){
+    응답.render('upload.ejs')
+  }); 
+  
+
+  let multer = require('multer');
+var storage = multer.diskStorage({
+
+  destination : function(req, file, cb){
+    cb(null, './public/image')
+  },
+  filename : function(req, file, cb){
+    cb(null, file.originalname )
+  }
+
+});
+
+var upload = multer({storage : storage});
+
+app.post('/upload', upload.single('프로필'), function(요청, 응답){
+  응답.send('업로드완료')
+}); 
+
+app.get('/image/:imageName', function(요청, 응답){
+  응답.sendFile( __dirname + '/public/image/' + 요청.params.imageName )
 })

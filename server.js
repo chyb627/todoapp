62b0bd130c4815b03438e6d8b501a4bbaadc0afc
cha <chyb627@gmail.com>
2021-11-17 17:00:37 +0900

    수정 : 검색 기능

--- server.js
+++ server.js
@@ -359,12 +359,12 @@ app.get('/fail', function (요청, 응답) {    //실패시 다시 회원가입 
 
 app.get('/search', (요청, 응답)=>{
   
-  console.log(요청.query.value);
+  console.log(요청.query);
   db.collection('post').find( { $text : { $search: 요청.query.value }} ).toArray((에러, 결과)=>{
     console.log(결과)
     응답.render('search.ejs', {posts : 결과})
   })
-})
+}) 
 
 //==================================================================================
 //=================================== chat.ejst ====================================
